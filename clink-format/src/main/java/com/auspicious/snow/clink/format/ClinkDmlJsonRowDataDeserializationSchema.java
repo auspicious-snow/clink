7d190bb3b09575706f942400307ee40469d95f28
chaixiaoxue <chaixiaoxue@yuanxin-inc.com>
2021-01-26 17:41:34 +0800

    init clink-format model

---
+++ clink-format/src/main/java/com/auspicious/snow/clink/format/ClinkDmlJsonRowDataDeserializationSchema.java
@@ -0,0 +1,165 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.auspicious.snow.clink.format;
+
+import static java.lang.String.format;
+import static org.apache.flink.util.Preconditions.checkNotNull;
+
+import java.io.IOException;
+import java.util.Objects;
+import org.apache.flink.annotation.Internal;
+import org.apache.flink.api.common.serialization.DeserializationSchema;
+import org.apache.flink.api.common.typeinfo.TypeInformation;
+import org.apache.flink.formats.json.TimestampFormat;
+import org.apache.flink.shaded.jackson2.com.fasterxml.jackson.databind.DeserializationFeature;
+import org.apache.flink.shaded.jackson2.com.fasterxml.jackson.databind.JsonNode;
+import org.apache.flink.shaded.jackson2.com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.flink.table.data.RowData;
+import org.apache.flink.table.types.logical.DecimalType;
+import org.apache.flink.table.types.logical.RowType;
+import org.apache.flink.table.types.logical.utils.LogicalTypeChecks;
+
+/**
+ * Deserialization schema from JSON to Flink Table/SQL internal data structure {@link RowData}.
+ *
+ * <p>Deserializes a <code>byte[]</code> message as a JSON object and reads the specified fields.
+ *
+ * <p>Failures during deserialization are forwarded as wrapped IOExceptions.
+ */
+@Internal
+public class ClinkDmlJsonRowDataDeserializationSchema implements DeserializationSchema<RowData> {
+    private static final long serialVersionUID = 1L;
+
+    /** Flag indicating whether to fail if a field is missing. */
+    private final boolean failOnMissingField;
+
+    /** Flag indicating whether to ignore invalid fields/rows (default: throw an exception). */
+    private final boolean ignoreParseErrors;
+
+    /** TypeInformation of the produced {@link RowData}. */
+    private final TypeInformation<RowData> resultTypeInfo;
+
+    /**
+     * Runtime converter that converts {@link JsonNode}s into objects of Flink SQL internal data
+     * structures.
+     */
+    private final ClinkDmlJsonToRowDataConverters.YxJsonToRowDataConverter runtimeConverter;
+
+    private final ClinkDmlJsonToRowDataConverters.YxJsonToRowDataConverter infoRuntimeConverter;
+
+    /** Object mapper for parsing the JSON. */
+    private final ObjectMapper objectMapper = new ObjectMapper();
+
+    /** Timestamp format specification which is used to parse timestamp. */
+    private final TimestampFormat timestampFormat;
+
+    private static final String MESSAGE = "message";
+    private  RowType rowType ;
+
+    public ClinkDmlJsonRowDataDeserializationSchema(
+            RowType rowType,
+            RowType infoType,
+            TypeInformation<RowData> resultTypeInfo,
+            boolean failOnMissingField,
+            boolean ignoreParseErrors,
+            TimestampFormat timestampFormat) {
+        if (ignoreParseErrors && failOnMissingField) {
+            throw new IllegalArgumentException(
+                    "JSON format doesn't support failOnMissingField and ignoreParseErrors are both enabled.");
+        }
+        this.resultTypeInfo = checkNotNull(resultTypeInfo);
+        this.failOnMissingField = failOnMissingField;
+        this.ignoreParseErrors = ignoreParseErrors;
+        this.timestampFormat = timestampFormat;
+        this.runtimeConverter =
+            new ClinkDmlJsonToRowDataConverters(failOnMissingField, ignoreParseErrors, timestampFormat)
+                .createConverter(checkNotNull(rowType));
+        this.infoRuntimeConverter =
+            new ClinkDmlJsonToRowDataConverters(failOnMissingField, ignoreParseErrors, timestampFormat)
+                .createConverter(checkNotNull(infoType));
+        this.rowType = rowType;
+        boolean hasDecimalType =
+                LogicalTypeChecks.hasNested(rowType, t -> t instanceof DecimalType);
+        if (hasDecimalType) {
+            objectMapper.enable(DeserializationFeature.USE_BIG_DECIMAL_FOR_FLOATS);
+        }
+    }
+
+    @Override
+    public RowData deserialize(byte[] message) throws IOException {
+        try {
+            JsonNode jsonNode = objectMapper.readTree(message);
+            String messageJson = jsonNode.get(MESSAGE).asText();
+            System.out.println(messageJson);
+            JsonNode messageNode = objectMapper.readTree(messageJson);
+
+            //jsonNode.get("batch_id").asText();
+            return (RowData) runtimeConverter.convert(messageNode);
+        } catch (Throwable t) {
+            if (ignoreParseErrors) {
+                return null;
+            }
+            throw new IOException(
+                    format("Failed to deserialize JSON '%s'.", new String(message)), t);
+        }
+    }
+
+    public RowData deserialize(byte[] message,Boolean flag) throws IOException {
+        try {
+            JsonNode jsonNode = objectMapper.readTree(message);
+            return (RowData) infoRuntimeConverter.convert(jsonNode);
+        } catch (Throwable t) {
+            if (ignoreParseErrors) {
+                return null;
+            }
+            throw new IOException(
+                format("Failed to deserialize JSON '%s'.", new String(message)), t);
+        }
+    }
+
+    @Override
+    public boolean isEndOfStream(RowData nextElement) {
+        return false;
+    }
+
+    @Override
+    public TypeInformation<RowData> getProducedType() {
+        return resultTypeInfo;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        ClinkDmlJsonRowDataDeserializationSchema that = (ClinkDmlJsonRowDataDeserializationSchema) o;
+        return failOnMissingField == that.failOnMissingField
+                && ignoreParseErrors == that.ignoreParseErrors
+                && resultTypeInfo.equals(that.resultTypeInfo)
+                && timestampFormat.equals(that.timestampFormat);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(failOnMissingField, ignoreParseErrors, resultTypeInfo, timestampFormat);
+    }
+}
